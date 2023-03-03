f5fb210cbdc8fcb093eab76f73eaa2ed4b9b8143
Matt Christiansen <39605194+pythagoras-19@users.noreply.github.com>
2023-03-02 18:53:50 -0800

    Update university.sql
    updated to fit requirements of uniqueness

--- university/university.sql
+++ university/university.sql
@@ -1,43 +1,40 @@
--- START OF Assignment 3
-CREATE database CSC174;
-use CSC174;
-CREATE table Student(
-	SSN CHAR(9) NOT NULL,
+CREATE TABLE Student(
+    SSN CHAR(9) UNIQUE NOT NULL,
     StudentName VARCHAR(100) NOT NULL,
     Address VARCHAR(50) NOT NULL,
     Email VARCHAR(50) NOT NULL,
     PRIMARY KEY (SSN)
 );
 CREATE TABLE TA(
-	SSN CHAR(9) NOT NULL,
+    SSN CHAR(9) NOT NULL,
     Salary DECIMAL(15,2) NOT NULL,
     PRIMARY KEY(SSN),
     FOREIGN KEY (SSN) REFERENCES Student(SSN)
 ); 
 CREATE TABLE Classroom(
-	RoomNo VARCHAR(50) NOT NULL,
+    RoomNo VARCHAR(50) NOT NULL,
     Building VARCHAR(50) NOT NULL,
     Capacity CHAR(10) NOT NULL,
     PRIMARY KEY (RoomNo, Building)
 );
 CREATE TABLE Instructor(
-	Title VARCHAR(100) NOT NULL,
-    InstructorID VARCHAR(50) NOT NULL,
+    Title VARCHAR(100) NOT NULL,
+    InstructorID VARCHAR(50) UNIQUE NOT NULL,
     InstructorName VARCHAR(100) NOT NULL,
     PRIMARY KEY (InstructorID)
 );
 CREATE TABLE Course(
-	CourseNo VARCHAR(25) NOT NULL,
+    CourseNo VARCHAR(25) UNIQUE NOT NULL,
     CourseName VARCHAR(50) NOT NULL,
     NoOfStudents CHAR(25) NOT NULL,
     InstructorID VARCHAR(50) NOT NULL,
-    TASSN CHAR(9), -- MAYBE NEEDS TO BE NOT NULL
+    TASSN CHAR(9),
     PRIMARY KEY (CourseNo),
     FOREIGN KEY (InstructorID) REFERENCES Instructor(InstructorID),
     FOREIGN KEY (TASSN) REFERENCES TA(SSN)
 );
 CREATE TABLE Enrolled(
-	SSN CHAR(9) NOT NULL,
+    SSN CHAR(9) NOT NULL,
     CourseNo VARCHAR(25) NOT NULL,
     Grade VARCHAR(2) NOT NULL,
     PRIMARY KEY(SSN, CourseNo),
@@ -45,13 +42,13 @@ CREATE TABLE Enrolled(
     FOREIGN KEY(SSN) REFERENCES Student(SSN)
 );
 CREATE TABLE OnlineCourse(
-	CourseNo VARCHAR(25) NOT NULL,
+    CourseNo VARCHAR(25) NOT NULL,
     URL VARCHAR(100) NOT NULL,
     PRIMARY KEY (CourseNo),
     FOREIGN KEY (CourseNo) REFERENCES Course(CourseNo)
 );
 CREATE TABLE InPersonCourse(
-	CourseNo VARCHAR(25) NOT NULL,
+    CourseNo VARCHAR(25) NOT NULL,
     RoomNo VARCHAR(50) NOT NULL,
     Building VARCHAR(50) NOT NULL,
     ClassTime VARCHAR(10) NOT NULL,
@@ -69,19 +66,22 @@ INSERT INTO Instructor VALUES(
 	'Professor', '1234A', 'Matt Damon'
 );
 INSERT INTO TA VALUES(
-	'123456789', '15000'
+	'123456789', '15000.00'
 );
 INSERT INTO Course VALUES(
 	'CSC130', 'Data Structures and Algorithms', '100', '1234A', '123456789'
 );
 INSERT INTO Course VALUES(
-	'CSC131', 'Software Engineering', '65', '1234A', '123456789'
+	'CSC131', 'Software Engineering', '85', '1234A', '123456789'
 );
 INSERT INTO Course VALUES(
-	'CSC137', 'Computer Architecture', '250', '1234A', NULL
+	'CSC137', 'Computer Architecture', '95', '1234A', NULL
 );
 INSERT INTO InPersonCourse VALUES(
 	'CSC131', '123A', 'Riverside', '0800' 
+);
+INSERT INTO InPersonCourse VALUES(
+	'CSC137', '123A', 'Riverside', '0915' 
 );
 INSERT INTO OnlineCourse VALUES(
 	'CSC130', 'https://zoom.us' 
@@ -110,7 +110,10 @@ WHERE C.CourseNo = T.CourseNo;
 
 SELECT * FROM InPersonCourseView;
 
--- Drop tables
+-- Drop views and tables
+DROP VIEW IF EXISTS TAView;
+DROP VIEW IF EXISTS OnlineCourseView;
+DROP VIEW IF EXISTS InPersonCourseView;
 DROP TABLE InPersonCourse;
 DROP TABLE Classroom;
 DROP TABLE OnlineCourse;
